3660ddb66a2af9cbac96284a1712c8a44811f8ae
= <=>
2023-01-04 19:13:58 +0300

    Initial Commit

---
+++ Ctructures/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Ctructures
+//
+//  Created by Özgür Kamalı on 4.01.2023.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
